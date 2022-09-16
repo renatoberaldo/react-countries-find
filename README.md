00256d03306cab5a1b707c3fd572906755316110
Renato Nunes <renatoberaldo@gmail.com>
2022-09-16 10:18:22 -0600

    Created README and deleted flag countries

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# react-countries
+# react-countries-find
+
+## About the app:
+
+This basic app shows more info (name, capital, region, population, area, demographic density) about the countries. All information is caught by a public API after writing at least 3 characters in the input.
+
+## Technologies and Packages:
+
+- ReactJS
+- Axios (get information by API)
+- Tailwind (layout)
+
+## How to install:
+
+```
+1 - npm install
+2 - yarn dev
+3 - Open the browser at http://localhost:3000/
+4 - Write at least 3 characteres in the input
+```
+
+## Created by:
+
+Renato Beraldo Nunes (me)
--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,25 +1,35 @@
-import Item from "./Item";
+import Item from './Item';
 
-export default function Country({children: country =[]}){
-  
-  if(!country) {
-    return <div>Impossible to render the country!!</div>
+export default function Country({ children: country = [] }) {
+  if (!country) {
+    return <div>Impossible to render the country!!</div>;
   }
-  
-  const demographicDensity = country.population/country.area;
-  const {flag, name, capital, region, population, area} = country;
 
-  return(
+  const demographicDensity = country.population / country.area;
+  const { name, capital, region, population, area } = country;
+
+  return (
     <div className="border p-2 m-2 flex flex-row items-center space-x-2">
-      {/* <img src={flag} alt={name} className="w-48" /> */}
       <ul>
-        <li><Item label="Name:">{name}</Item></li>
-        <li><Item label="Capital:">{capital}</Item></li>
-        <li><Item label="Region:">{region}</Item></li>
-        <li><Item label="Population:">{population}</Item></li>
-        <li><Item label="Area:">{area}</Item></li>
-        <li><Item label="Demographic Density:">{demographicDensity}</Item></li>
+        <li>
+          <Item label="Name:">{name}</Item>
+        </li>
+        <li>
+          <Item label="Capital:">{capital}</Item>
+        </li>
+        <li>
+          <Item label="Region:">{region}</Item>
+        </li>
+        <li>
+          <Item label="Population:">{population}</Item>
+        </li>
+        <li>
+          <Item label="Area:">{area}</Item>
+        </li>
+        <li>
+          <Item label="Demographic Density:">{demographicDensity}</Item>
+        </li>
       </ul>
     </div>
-  )
+  );
 }
